7515cadd471de6ca4dc732adfe609690a53bd1b7
Bernard <bernard@zulzi.com>
2020-04-11 14:13:42 +0200

    Updates

---
+++ precache-manifest.e67b22da13ed728cee8400e60681f7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8c4838ddab8cf1b0bc7db413ac2d0e",
+    "url": "/lap-timer/index.html"
+  },
+  {
+    "revision": "cf3de5e1b59b65575cd0",
+    "url": "/lap-timer/static/css/main.6169ceb5.chunk.css"
+  },
+  {
+    "revision": "5877627c285bc496e3c0",
+    "url": "/lap-timer/static/js/2.eb04fb3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lap-timer/static/js/2.eb04fb3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf3de5e1b59b65575cd0",
+    "url": "/lap-timer/static/js/main.cf958aef.chunk.js"
+  },
+  {
+    "revision": "c791a208a724f65d7bef",
+    "url": "/lap-timer/static/js/runtime-main.0bdb79eb.js"
+  }
+]);
